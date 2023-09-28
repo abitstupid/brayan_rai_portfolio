e3b468b684cbdb805a7b79eb3edca6b47c237957
abitstupid <hrhemant0000at@gmail.com>
2023-09-29 01:02:14 +0530

    navbar and responsive fixes

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,5 @@ contactForm.addEventListener("submit", function (e) {
 	e.preventDefault();
 	this.submit();
 
-    window.location.href = "http://127.0.0.1:5500/index.html#contact";
+	window.location.href = "http://127.0.0.1:5500/index.html#contact";
 });
